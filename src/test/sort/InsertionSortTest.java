22f9a16d11de1e22b1af06ce103a92b4685a205e
Felipe Knop <felipebarraknop@yahoo.com.br>
2017-08-07 20:18:46 -0300

    Alteração nas classes de teste para aumentar a reusabilidade do código

--- src/test/sort/InsertionSortTest.java
+++ src/test/sort/InsertionSortTest.java
@@ -2,6 +2,7 @@ package test.sort;
 
 import org.junit.Test;
 import sort.InsertionSort;
+import sort.SortingAlgorithm;
 import test.TestUtils;
 
 import java.util.List;
@@ -19,6 +20,11 @@ public class InsertionSortTest {
     private static final int DEFAULT_SIZE = 10000;
 
     /**
+     * Instância do algoritmo de sort a ser utilizado.
+     */
+    private static final SortingAlgorithm sortingAlgorithm = new InsertionSort();
+
+    /**
      * Testa a ordenação de uma array de inteiros aleatórios gerada pela classe
      * {@link TestUtils}. Caso algum elemento da array seja maior que o elemento
      * anterior, o teste falha, imprimindo quais são os elementos que falharam.
@@ -27,8 +33,7 @@ public class InsertionSortTest {
     @Test
     public void sortRandomIntegerArray() {
         Integer[] array = TestUtils.getRandomIntegerArray(DEFAULT_SIZE);
-        InsertionSort insertionSort = new InsertionSort();
-        Integer[] sorted = insertionSort.sort(array);
+        Integer[] sorted = sortingAlgorithm.sort(array);
         for (int i = 1; i < sorted.length - 1; i++)
             if (sorted[i - 1].compareTo(sorted[i]) > 0)
                 fail(String.format("A array não está ordenada corretamente. Índice [%d]: %d, índice [%d]: %d", i - 1, sorted[i - 1], i, sorted[i]));
@@ -44,8 +49,7 @@ public class InsertionSortTest {
     @Test
     public void sortRandomIntegerList() {
         List<Integer> list = TestUtils.getRandomIntegerList(DEFAULT_SIZE);
-        InsertionSort insertionSort = new InsertionSort();
-        List<Integer> sorted = insertionSort.sort(list);
+        List<Integer> sorted = sortingAlgorithm.sort(list);
         for (int i = 1; i < sorted.size() - 1; i++)
             if (sorted.get(i - 1).compareTo(sorted.get(i)) > 0)
                 fail(String.format("A lista não está ordenada corretamente. Índice [%d]: %d, índice [%d]: %d", i - 1, sorted.get(i - 1), i, sorted.get(i)));
@@ -61,8 +65,7 @@ public class InsertionSortTest {
     @Test
     public void sortRandomStringArray() {
         String[] array = TestUtils.getRandomStringArray(DEFAULT_SIZE);
-        InsertionSort insertionSort = new InsertionSort();
-        String[] sorted = insertionSort.sort(array);
+        String[] sorted = sortingAlgorithm.sort(array);
         for (int i = 1; i < sorted.length - 1; i++)
             if (sorted[i - 1].compareTo(sorted[i]) > 0)
                 fail(String.format("A array não está ordenada corretamente. Índice [%d]: %s, índice [%d]: %s", i - 1, sorted[i - 1], i, sorted[i]));
@@ -78,8 +81,7 @@ public class InsertionSortTest {
     @Test
     public void sortRandomStringList() {
         List<String> list = TestUtils.getRandomStringList(DEFAULT_SIZE);
-        InsertionSort insertionSort = new InsertionSort();
-        List<String> sorted = insertionSort.sort(list);
+        List<String> sorted = sortingAlgorithm.sort(list);
         for (int i = 1; i < sorted.size() - 1; i++)
             if (sorted.get(i - 1).compareTo(sorted.get(i)) > 0)
                 fail(String.format("A lista não está ordenada corretamente. Índice [%d]: %s, índice [%d]: %s", i - 1, sorted.get(i - 1), i, sorted.get(i)));
