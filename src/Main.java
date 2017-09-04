924d8e7b5548042f98da13940bfba5a0647e10a2
Felipe Knop <felipebarraknop@yahoo.com.br>
2017-09-04 00:49:22 -0300

    Implementada função principal do projeto que faz leitura do arquivo de entrada e escreve no arquivo de saída os resultados necessários

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,101 @@
+import business.TweetFileReader;
+import sort.BenchmarkSortingAlgorithm;
+import sort.QuickSort;
+
+import java.io.*;
+
 public class Main {
 
+    /**
+     * Método main que lê os valores do arquivo de entrada, cria
+     * O TweetFileReader que lê o arquivo de Tweets, redireciona
+     * a saída para o arquivo saida.txt e executa as funções pedidas
+     * na especificação do trabalho.
+     * @param args Parâmetros da linha de comando
+     */
     public static void main(String[] args) {
+        int[] nValues = readInputFile();
+        assert nValues != null;
+        TweetFileReader tweetFileReader = createTweetFileReader();
+        assert tweetFileReader != null;
+        redirectOutput();
+        cenario1(nValues, tweetFileReader);
+    }
 
+    /**
+     * Executa as funções pedidas no Cenário 1 da especificação do trabalho.
+     * @param nValues Valores lidos do arquivo de entrada
+     * @param tweetFileReader TweetFileReader com lista de Tweets preenchida
+     */
+    private static void cenario1(int[] nValues, TweetFileReader tweetFileReader) {
+        BenchmarkSortingAlgorithm.benchmarkIntegers(new QuickSort(), nValues, tweetFileReader);
+        BenchmarkSortingAlgorithm.benchmarkTweets(new QuickSort(), nValues, tweetFileReader);
+    }
+
+    /**
+     * Lê o arquivo de entrada no formato descrito no arquivo Trabalho1.pdf:
+     *
+     * 7 -> número de valores de N que se seguem, por linha
+     * 1000
+     * 5000
+     * 10000
+     *  .
+     *  .
+     *  .
+     *
+     * @return Array de inteiros com os valores de N lidos
+     */
+    private static int[] readInputFile() {
+        File file = new File("entrada.txt");
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            int nAmount = Integer.parseInt(bufferedReader.readLine());
+            int[] nValues = new int[nAmount];
+            for (int i = 0; i < nAmount; i++)
+                nValues[i] = Integer.parseInt(bufferedReader.readLine());
+            bufferedReader.close();
+            return nValues;
+        } catch (FileNotFoundException e) {
+            System.out.println("Não foi possível encontrar o arquivo de entrada. Certifique-se" +
+                    " de que o nome dele é entrada.txt e que ele está na pasta base (Estrutura de Dados II)");
+            System.exit(0);
+            return null;
+        } catch (IOException e) {
+            System.out.println("Falha ao ler o arquivo de entrada.\n" + e.getMessage());
+            System.exit(0);
+            return null;
+        }
+    }
+
+    /**
+     * Redireciona para o arquivo saida.txt tudo que é passado à função
+     * {@link System#out#println()}.
+     */
+    private static void redirectOutput() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("saida.txt");
+            System.setOut(new PrintStream(fileOutputStream));
+        } catch (FileNotFoundException e) {
+            System.out.println("Não foi possível criar o arquivo de saída.\n" + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Cria TweetFileReader para arquivo tweets.txr, que contém uma lista de
+     * 1 milhão de Tweets obtidos no link contido no arquivo Trabalho1.pdf.
+     * @return Retorna uma instância de TweetFileReader com o arquivo
+     * de Tweets já lido a lista de Tweets já gerada e preenchida
+     */
+    private static TweetFileReader createTweetFileReader() {
+        try {
+            return TweetFileReader.create("tweets.txt");
+        } catch (IOException e) {
+            System.out.println("Não foi possível encontrar o arquivo de Tweets. Certifique-se" +
+                    " de que o nome dele é tweets.txt e que ele está na pasta base (Estrutura de Dados II)");
+            System.exit(0);
+            return null;
+        }
     }
 }
