d68f366623acb177c3eeb1d78810461b071026ff
Lohan-Ferreira <lohanext@gmail.com>
2017-09-10 07:12:13 -0300

    correção no duplo hash

--- src/hash/collision_resolution/DoubleHashingMethod.java
+++ src/hash/collision_resolution/DoubleHashingMethod.java
@@ -7,6 +7,6 @@ public class DoubleHashingMethod implements CollisionResolutionMethod {
 
     public int resolveFunction(int value, int failedIndex, int iteration, int mod) {
         HashingAlgorithm rehash = new MultiplicationHash();
-        return (failedIndex + iteration*( 1+ rehash.hashFunction(value,mod)));
+        return (failedIndex + iteration*( 1+ rehash.hashFunction(value,mod))) % mod ;
     }
 }
