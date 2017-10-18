e5d0a79ac3bbacaed365ffa1a0fca17202edb50b
FelipeKnop <felipebarraknop@yahoo.com.br>
2017-10-18 15:14:57 -0200

    Correções na AVLTree e implementação da remoção

--- src/tree/AVLTree.java
+++ src/tree/AVLTree.java
@@ -4,19 +4,22 @@ public class AVLTree<T extends Comparable<? super T>> {
 
     public static void main(String[] args) {
         AVLTree<Integer> avlTree = new AVLTree<>();
-        avlTree.insert(5);
-        avlTree.insert(3);
         avlTree.insert(7);
-        avlTree.insert(2);
-        avlTree.insert(8);
-        avlTree.insert(1);
-        avlTree.insert(9);
-        avlTree.insert(5);
-        avlTree.insert(6);
+        avlTree.insert(14);
         avlTree.insert(4);
+        avlTree.insert(12);
+        avlTree.insert(15);
+        avlTree.insert(2);
+        avlTree.insert(6);
+        avlTree.insert(11);
+        avlTree.insert(13);
+        avlTree.insert(1);
         avlTree.insert(3);
-        Integer result = avlTree.search(7);
-        result = avlTree.search(10);
+        avlTree.insert(5);
+        Integer result = avlTree.search(4);
+        avlTree.remove(4);
+        result = avlTree.search(4);
+        int i = 0;
     }
 
     private Node root;
@@ -27,10 +30,12 @@ public class AVLTree<T extends Comparable<? super T>> {
     }
 
     private void rotationLeft(Node head) {
-        if (head.equals(head.parent.leftChild)){
-            head.parent.leftChild = head.rightChild;
-        } else {
-            head.parent.rightChild = head.rightChild;
+        if (!head.equals(root)) {
+            if (head.equals(head.parent.leftChild)) {
+                head.parent.leftChild = head.rightChild;
+            } else {
+                head.parent.rightChild = head.rightChild;
+            }
         }
 
         head.rightChild.parent = head.parent;
@@ -41,11 +46,12 @@ public class AVLTree<T extends Comparable<? super T>> {
     }
 
     private void rotationRight(Node head) {
-
-        if (head.equals(head.parent.leftChild)){
-            head.parent.leftChild = head.leftChild;
-        } else {
-            head.parent.rightChild = head.leftChild;
+        if (!head.equals(root)) {
+            if (head.equals(head.parent.leftChild)) {
+                head.parent.leftChild = head.leftChild;
+            } else {
+                head.parent.rightChild = head.leftChild;
+            }
         }
 
         head.leftChild.parent = head.parent;
@@ -111,9 +117,6 @@ public class AVLTree<T extends Comparable<? super T>> {
     }
 
     public T search (T key) {
-        if (root == null)
-            return null;
-
         Node aux = root;
         while (true) {
             if (aux == null)
@@ -125,6 +128,63 @@ public class AVLTree<T extends Comparable<? super T>> {
                 aux = aux.rightChild;
             } else
                 return aux.key;
+        }
+    }
+
+    public boolean remove (T key) {
+        Node aux = root;
+        while (true) {
+            if (aux == null)
+                return false;
+
+            if (key.compareTo(aux.key) < 0) {
+                aux = aux.leftChild;
+            } else if (key.compareTo(aux.key) > 0) {
+                aux = aux.rightChild;
+            } else {
+                Node parent = aux.parent;
+                if (aux.leftChild == null && aux.rightChild == null) {
+                    if (parent.leftChild.equals(aux)) parent.leftChild = null;
+                    else parent.rightChild = null;
+                } else if (aux.leftChild != null ^ aux.rightChild != null) {
+                    if (parent.leftChild.equals(aux))
+                        if (aux.leftChild != null) {
+                            parent.leftChild = aux.leftChild;
+                            aux.leftChild.parent = parent;
+                        } else {
+                            parent.leftChild = aux.rightChild;
+                            aux.rightChild.parent = parent;
+                        }
+                    else
+                        if (aux.leftChild != null) {
+                            parent.rightChild = aux.leftChild;
+                            aux.leftChild.parent = parent;
+                        } else {
+                            parent.rightChild = aux.rightChild;
+                            aux.rightChild.parent = parent;
+                        }
+                } else {
+                    Node aux2 = aux.leftChild;
+                    while (aux2.rightChild != null)
+                        aux2 = aux2.rightChild;
+                    if (parent.leftChild.equals(aux)) aux.parent.leftChild = aux2;
+                    else aux.parent.rightChild = aux2;
+                    parent = aux2.parent;
+                    aux2.parent = aux.parent;
+                    if (!aux.leftChild.equals(aux2)) {
+                        aux2.leftChild = aux.leftChild;
+                        aux2.leftChild.parent = aux2;
+                        parent.rightChild = null;
+                    }
+                    aux2.rightChild = aux.rightChild;
+                    aux2.rightChild.parent = aux2;
+                }
+                while (parent != null) {
+                    rebalance(parent);
+                    parent = parent.parent;
+                }
+                return true;
+            }
         }
     }
 
