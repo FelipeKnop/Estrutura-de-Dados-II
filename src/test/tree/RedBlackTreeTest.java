5ba7bc8fb080ee438c7da5ea5ddd8d5e4a154851
Felipe Knop <felipebarraknop@yahoo.com.br>
2017-11-01 22:44:42 -0200

    RedBlackTree agora extende BinarySearchTree e RedBlackTreeTest extende BinarySearchTreeTest

--- src/test/tree/RedBlackTreeTest.java
+++ src/test/tree/RedBlackTreeTest.java
@@ -1,102 +1,38 @@
 package test.tree;
 
-import org.junit.Test;
+import tree.BinarySearchTree;
 import tree.RedBlackTree;
 
-import java.util.List;
+public class RedBlackTreeTest extends BinarySearchTreeTest {
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-public class RedBlackTreeTest {
-
-    private static final Integer[] TEST_ARRAY = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-    private static final Integer[] LEVEL_ORDER_ARRAY = new Integer[] {4, 2, 8, 1, 3, 6, 10, 5, 7, 9, 12, 11, 14, 13, 15};
-
-    @Test
-    public void insertTest() {
-        RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
-
-        for (Integer number : TEST_ARRAY)
-            redBlackTree.insert(number);
-
-        assertArrayEquals(getLevelOrderElements(redBlackTree), LEVEL_ORDER_ARRAY);
+    @Override
+    protected Integer[] getLevelOrderArray() {
+        return new Integer[] {4, 2, 8, 1, 3, 6, 10, 5, 7, 9, 12, 11, 14, 13, 15};
     }
 
-    @Test
-    public void searchTest() {
-        RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
-
-        for (Integer number : TEST_ARRAY)
-            assertEquals(redBlackTree.search(number), null);
-
-        for (Integer number : TEST_ARRAY)
-            redBlackTree.insert(number);
-
-        for (Integer number : TEST_ARRAY)
-            assertEquals(redBlackTree.search(number), number);
-
-        assertArrayEquals(getLevelOrderElements(redBlackTree), LEVEL_ORDER_ARRAY);
+    @Override
+    protected BinarySearchTree<Integer> getNewBinarySearchTree() {
+        return new RedBlackTree<>();
     }
 
-    @Test
-    public void removeTest() {
-        RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
-
-        for (Integer number : TEST_ARRAY)
-            assertEquals(redBlackTree.remove(number), false);
-
-        for (Integer number : TEST_ARRAY)
-            redBlackTree.insert(number);
-
-        redBlackTree.remove(5);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {4, 2, 10, 1, 3, 8, 12, 6, 9, 11, 14, 7, 13, 15});
-
-        redBlackTree.remove(2);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {10, 4, 12, 1, 8, 11, 14, 3, 6, 9, 13, 15, 7});
-
-        redBlackTree.remove(12);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {10, 4, 14, 1, 8, 11, 15, 3, 6, 9, 13, 7});
-
-        redBlackTree.remove(6);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {10, 4, 14, 1, 8, 11, 15, 3, 7, 9, 13});
-
-        redBlackTree.remove(10);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 4, 14, 1, 8, 11, 15, 3, 7, 13});
-
-        redBlackTree.remove(4);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 3, 14, 1, 8, 11, 15, 7, 13});
-
-        redBlackTree.remove(11);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 3, 14, 1, 8, 13, 15, 7});
-
-        redBlackTree.remove(8);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 3, 14, 1, 7, 13, 15});
-
-        redBlackTree.remove(14);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 3, 13, 1, 7, 15});
-
-        redBlackTree.remove(13);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 3, 15, 1, 7});
-
-        redBlackTree.remove(7);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9, 3, 15, 1});
-
-        redBlackTree.remove(15);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {3, 1, 9});
-
-        redBlackTree.remove(3);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {1, 9});
-
-        redBlackTree.remove(1);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {9});
-
-        redBlackTree.remove(9);
-        assertArrayEquals(getLevelOrderElements(redBlackTree), new Integer[] {});
-    }
-
-    private Integer[] getLevelOrderElements(RedBlackTree<Integer> redBlackTree) {
-        List<Integer> elements = redBlackTree.levelOrderTraversal();
-        return elements.toArray(new Integer[elements.size()]);
+    @Override
+    protected Integer[][] getRemoveLevelOrderArrays() {
+        return new Integer[][] {
+                {4, 2, 10, 1, 3, 8, 12, 6, 9, 11, 14, 7, 13, 15},
+                {10, 4, 12, 1, 8, 11, 14, 3, 6, 9, 13, 15, 7},
+                {10, 4, 14, 1, 8, 11, 15, 3, 6, 9, 13, 7},
+                {10, 4, 14, 1, 8, 11, 15, 3, 7, 9, 13},
+                {9, 4, 14, 1, 8, 11, 15, 3, 7, 13},
+                {9, 3, 14, 1, 8, 11, 15, 7, 13},
+                {9, 3, 14, 1, 8, 13, 15, 7},
+                {9, 3, 14, 1, 7, 13, 15},
+                {9, 3, 13, 1, 7, 15},
+                {9, 3, 15, 1, 7},
+                {9, 3, 15, 1},
+                {3, 1, 9},
+                {1, 9},
+                {9},
+                {}
+        };
     }
 }
--- src/tree/RedBlackTree.java
+++ src/tree/RedBlackTree.java
@@ -1,256 +1,38 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class RedBlackTree<T extends Comparable<? super T>> {
+public class RedBlackTree<T extends Comparable<? super T>> extends BinarySearchTree<T> {
 
     private static final boolean RED = false;
     private static final boolean BLACK = true;
 
-    public static void main(String[] args) {
-        RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
-        redBlackTree.insert(1);
-        redBlackTree.insert(2);
-        redBlackTree.insert(3);
-        redBlackTree.insert(4);
-        redBlackTree.insert(5);
-        redBlackTree.insert(6);
-        redBlackTree.insert(7);
-        int i = 0;
+    @Override
+    protected Node createNode(T key) {
+        return new RedBlackNode(key, BLACK);
     }
 
-    private Node root;
+    // TODO: Implementar a inserção e remoção de elementos
 
-    private int height(Node node) {
-        if (node == null) return 0;
-        return 1 + Math.max(height(node.leftChild), height(node.rightChild));
-    }
+    private class RedBlackNode extends Node {
 
-    private void rotationLeft(Node head) {
-        if (!head.equals(root)) {
-            if (head.equals(head.parent.leftChild)) {
-                head.parent.leftChild = head.rightChild;
-            } else {
-                head.parent.rightChild = head.rightChild;
-            }
-        }
-
-        head.rightChild.parent = head.parent;
-        head.parent = head.rightChild;
-        Node aux = head.rightChild;
-        head.rightChild = head.rightChild.leftChild;
-        aux.leftChild = head;
-
-        if (head.equals(root)) root = head.parent;
-    }
-
-    private void rotationRight(Node head) {
-        if (!head.equals(root)) {
-            if (head.equals(head.parent.leftChild)) {
-                head.parent.leftChild = head.leftChild;
-            } else {
-                head.parent.rightChild = head.leftChild;
-            }
-        }
-
-        head.leftChild.parent = head.parent;
-        head.parent = head.leftChild;
-        Node aux = head.leftChild;
-        head.leftChild = head.leftChild.rightChild;
-        aux.rightChild = head;
-
-        if (head.equals(root)) root = head.parent;
-    }
-
-    private void rotationLeftRight(Node head) {
-        rotationLeft(head.leftChild);
-        rotationRight(head);
-    }
-
-    private void rotationRightLeft(Node head) {
-        rotationRight(head.rightChild);
-        rotationLeft(head);
-    }
-
-    private void rebalance(Node node) {
-        if (node.parent.parent == null)
-            return;
-
-        Node grandparent = node.parent.parent;
-        Node parent = node.parent;
-        Node uncle;
-
-        if (parent.equals(grandparent.leftChild))
-            uncle = grandparent.rightChild;
-        else
-            uncle = grandparent.leftChild;
-
-        if (uncle != null && uncle.color == RED) {
-            uncle.color = BLACK;
-            parent.recolor();
-            grandparent.recolor();
-        } else {
-            if (parent.equals(grandparent.leftChild)) {
-                if (node.equals(parent.leftChild)) {
-                    rotationRight(grandparent);
-                    parent.recolor();
-                    grandparent.recolor();
-                } else {
-                    rotationLeftRight(grandparent);
-                    node.recolor();
-                    grandparent.recolor();
-                }
-            } else {
-                if (node.equals(parent.leftChild)) {
-                    rotationRightLeft(grandparent);
-                    node.recolor();
-                    grandparent.recolor();
-                } else {
-                    rotationLeft(grandparent);
-                    parent.recolor();
-                    grandparent.recolor();
-                }
-            }
-        }
-    }
-
-    public void insert(T value) {
-        if (root == null) {
-            root = new Node(value);
-            root.color = BLACK;
-            return;
-        }
-
-        Node aux = root;
-        while (true) {
-            if (value.compareTo(aux.key) < 0)
-                if (aux.leftChild == null) {
-                    aux.leftChild = new Node(value);
-                    aux.leftChild.parent = aux;
-                    rebalance(aux.leftChild);
-                    break;
-                } else
-                    aux = aux.leftChild;
-            else if (aux.rightChild == null) {
-                aux.rightChild = new Node(value);
-                aux.rightChild.parent = aux;
-                rebalance(aux.rightChild);
-                break;
-            } else
-                aux = aux.rightChild;
-        }
-
-        root.color = BLACK;
-    }
-
-    public T search (T key) {
-        Node aux = root;
-        while (true) {
-            if (aux == null)
-                return null;
-
-            if (key.compareTo(aux.key) < 0) {
-                aux = aux.leftChild;
-            } else if (key.compareTo(aux.key) > 0) {
-                aux = aux.rightChild;
-            } else
-                return aux.key;
-        }
-    }
-
-    //TODO
-    public boolean remove (T key) {
-//        Node aux = root;
-//        while (true) {
-//            if (aux == null)
-//                return false;
-//
-//            if (key.compareTo(aux.key) < 0) {
-//                aux = aux.leftChild;
-//            } else if (key.compareTo(aux.key) > 0) {
-//                aux = aux.rightChild;
-//            } else {
-//                Node parent = aux.parent;
-//                if (aux.leftChild == null && aux.rightChild == null) {
-//                    if (parent.leftChild.equals(aux)) parent.leftChild = null;
-//                    else parent.rightChild = null;
-//                } else if (aux.leftChild != null ^ aux.rightChild != null) {
-//                    if (parent.leftChild.equals(aux))
-//                        if (aux.leftChild != null) {
-//                            parent.leftChild = aux.leftChild;
-//                            aux.leftChild.parent = parent;
-//                        } else {
-//                            parent.leftChild = aux.rightChild;
-//                            aux.rightChild.parent = parent;
-//                        }
-//                    else
-//                    if (aux.leftChild != null) {
-//                        parent.rightChild = aux.leftChild;
-//                        aux.leftChild.parent = parent;
-//                    } else {
-//                        parent.rightChild = aux.rightChild;
-//                        aux.rightChild.parent = parent;
-//                    }
-//                } else {
-//                    Node aux2 = aux.leftChild;
-//                    while (aux2.rightChild != null)
-//                        aux2 = aux2.rightChild;
-//                    if (parent.leftChild.equals(aux)) aux.parent.leftChild = aux2;
-//                    else aux.parent.rightChild = aux2;
-//                    parent = aux2.parent;
-//                    aux2.parent = aux.parent;
-//                    if (!aux.leftChild.equals(aux2)) {
-//                        aux2.leftChild = aux.leftChild;
-//                        aux2.leftChild.parent = aux2;
-//                        parent.rightChild = null;
-//                    }
-//                    aux2.rightChild = aux.rightChild;
-//                    aux2.rightChild.parent = aux2;
-//                }
-//                while (parent != null) {
-//                    rebalance(parent);
-//                    parent = parent.parent;
-//                }
-//                return true;
-//            }
-//        }
-        return false;
-    }
-
-    public List<T> levelOrderTraversal() {
-        List<T> elements = new ArrayList<>();
-        int height = height(root);
-        for (int i = 1; i <= height; i++)
-            traverseLevel(root, i, elements);
-        return elements;
-    }
-
-    private void traverseLevel(Node node, int level, List<T> elements) {
-        if (node != null) {
-            if (level == 1) elements.add(node.key);
-            else if (level > 1) {
-                traverseLevel(node.leftChild, level - 1, elements);
-                traverseLevel(node.rightChild, level - 1, elements);
-            }
-        }
-    }
-
-    private class Node {
-
-        private T key;
-        private Node parent;
-        private Node leftChild;
-        private Node rightChild;
         private boolean color;
 
-        private Node(T key) {
-            this.key = key;
-            this.color = RED;
+        RedBlackNode(T key, boolean color) {
+            super(key);
+            this.color = color;
         }
 
-        private void recolor() {
-            this.color = !this.color;
+        RedBlackNode getGrandParent() {
+            return parent == null || parent.parent == null ? null : (RedBlackNode) parent.parent;
+        }
+
+        RedBlackNode getUncle() {
+            RedBlackNode grandParent = getGrandParent();
+            if (grandParent == null) return null;
+            if (grandParent.leftChild != null && grandParent.leftChild == parent)
+                return (RedBlackNode) grandParent.rightChild;
+            else if (grandParent.rightChild != null && grandParent.rightChild == parent)
+                return (RedBlackNode) grandParent.leftChild;
+            return null;
         }
     }
 }
