6217be759502af82874e40dd5f8d1dc21ae687a3
Lohan-Ferreira <lohanext@gmail.com>
2017-11-02 13:27:24 -0200

    Inserção na RedBlackTree

--- src/tree/RedBlackTree.java
+++ src/tree/RedBlackTree.java
@@ -10,8 +10,84 @@ public class RedBlackTree<T extends Comparable<? super T>> extends BinarySearchT
 
     @Override
     protected Node createNode(T key) {
-        return new RedBlackNode(key, BLACK);
+        return new RedBlackNode(key, RED);
     }
+
+
+    private void addHelper(RedBlackNode node)
+    {
+        if(node.parent==null)
+        {
+            node.color = BLACK;
+            return;
+        }
+        RedBlackNode uncle =  node.getUncle();
+        RedBlackNode grand = node.getGrandParent();
+        RedBlackNode parent = (RedBlackNode) node.parent;
+        if (node.color == RED && parent.color == RED) {
+            if (uncle != null && uncle.color == RED)
+            {
+                parent.color= !parent.color;
+                uncle.color = !uncle.color;
+                grand.color = !grand.color;
+            }
+            else
+            {
+                if(node==parent.leftChild && parent == grand.leftChild)
+                {
+                    parent.color = !parent.color;
+                    grand.color = !parent.color;
+                    rotateRight(grand);
+                }
+
+                else if(node==parent.rightChild && parent == grand.rightChild)
+                {
+                    parent.color = !parent.color;
+                    grand.color = !parent.color;
+                    rotateLeft(grand);
+                }
+
+                else if (node==parent.leftChild && parent == grand.rightChild)
+                {
+                    node.color =!node.color;
+                    grand.color =!grand.color;
+                    rotateRight(parent);
+                    rotateLeft(grand);
+
+                }
+
+                else if (node==parent.rightChild && parent == grand.leftChild)
+                {
+                    node.color =!node.color;
+                    grand.color =!grand.color;
+                    rotateLeft(parent);
+                    rotateRight(grand);
+                }
+            }
+
+
+
+        }
+        addHelper(parent);
+    }
+    @Override
+    protected Node addValue (T value)
+    {
+        Node node = super.addValue(value);
+        RedBlackNode newnode = (RedBlackNode) node;
+
+        if (newnode.parent == null) {
+            newnode.color = BLACK;
+            return newnode;
+        }
+        else {
+           addHelper(newnode);
+           int i =0;
+           return newnode;
+        }
+
+    }
+
 
     // TODO: Implementar a inserção e remoção de elementos
 
