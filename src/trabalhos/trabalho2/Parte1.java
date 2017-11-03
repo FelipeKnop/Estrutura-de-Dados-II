4eb329b1e2b601a920ea903f40b371e89dfbc552
Felipe Knop <felipebarraknop@yahoo.com.br>
2017-11-03 14:28:28 -0200

    Criada classe BenchmarkTreeStructure e sua utilização na Parte 1 do Trabalho 2

--- src/trabalhos/trabalho2/Parte1.java
+++ src/trabalhos/trabalho2/Parte1.java
@@ -1,6 +1,9 @@
 package trabalhos.trabalho2;
 
 import business.TweetFileReader;
+import tree.AVLTree;
+import tree.BenchmarkTreeStructure;
+import tree.RedBlackTree;
 
 import static business.FileManager.readInputFile;
 import static business.FileManager.redirectOutput;
@@ -35,14 +38,35 @@ public class Parte1 {
     }
 
     private static void insercao(int[] nValues, TweetFileReader tweetFileReader) {
-
+        System.out.println("\n\n-------------- Árvore AVL --------------");
+        BenchmarkTreeStructure.benchmarkInsertTweets(new AVLTree<>(), nValues, tweetFileReader);
+        System.out.println("\n\n-------------- Árvore Vermelho e Preta --------------");
+        BenchmarkTreeStructure.benchmarkInsertTweets(new RedBlackTree<>(), nValues, tweetFileReader);
+//        System.out.println("\n\n-------------- Árvore Splay --------------");
+//        BenchmarkTreeStructure.benchmarkInsertTweets(new SplayTree<>(), nValues, tweetFileReader);
+//        System.out.println("\n\n-------------- Árvore B --------------");
+//        BenchmarkTreeStructure.benchmarkInsertTweets(new BTree<>(), nValues, tweetFileReader);
     }
 
     private static void busca(int[] nValues, TweetFileReader tweetFileReader) {
-
+        System.out.println("\n\n-------------- Árvore AVL --------------");
+        BenchmarkTreeStructure.benchmarkSearchTweets(new AVLTree<>(), nValues, tweetFileReader);
+        System.out.println("\n\n-------------- Árvore Vermelho e Preta --------------");
+        BenchmarkTreeStructure.benchmarkSearchTweets(new RedBlackTree<>(), nValues, tweetFileReader);
+//        System.out.println("\n\n-------------- Árvore Splay --------------");
+//        BenchmarkTreeStructure.benchmarkSearchTweets(new SplayTree<>(), nValues, tweetFileReader);
+//        System.out.println("\n\n-------------- Árvore B --------------");
+//        BenchmarkTreeStructure.benchmarkSearchTweets(new BTree<>(), nValues, tweetFileReader);
     }
 
     private static void remocao(int[] nValues, TweetFileReader tweetFileReader) {
-
+        System.out.println("\n\n-------------- Árvore AVL --------------");
+        BenchmarkTreeStructure.benchmarkRemoveTweets(new AVLTree<>(), nValues, tweetFileReader);
+        System.out.println("\n\n-------------- Árvore Vermelho e Preta --------------");
+        BenchmarkTreeStructure.benchmarkRemoveTweets(new RedBlackTree<>(), nValues, tweetFileReader);
+//        System.out.println("\n\n-------------- Árvore Splay --------------");
+//        BenchmarkTreeStructure.benchmarkRemoveTweets(new SplayTree<>(), nValues, tweetFileReader);
+//        System.out.println("\n\n-------------- Árvore B --------------");
+//        BenchmarkTreeStructure.benchmarkRemoveTweets(new BTree<>(), nValues, tweetFileReader);
     }
 }
---
+++ src/tree/BenchmarkTreeStructure.java
@@ -0,0 +1,131 @@
+package tree;
+
+import business.TweetFileReader;
+import models.Tweet;
+
+import static business.BenchmarkHelper.AMOUNT_OF_SETS;
+import static business.BenchmarkHelper.generateTweetArrays;
+
+/**
+ * Classe para análise de estruturas de árvore.
+ */
+public class BenchmarkTreeStructure {
+
+    /**
+     * Método que analisa a inserção em uma estrutura de árvore com os valores dados de entrada.
+     *
+     * A array nValues contém os valores de N (quantidade de elementos a serem inseridos)
+     * a serem utilizados na análise.
+     *
+     * A variável AMOUNT_OF_SETS é utilizada para determinar quantos testes
+     * serão realizados com diferentes sementes e contabilizados na média
+     * dos resultados obtidos.
+     *
+     * @param tree Estrutura de árvore a ser analisada
+     * @param nValues Valores de N (tamanho da array)
+     * @param tweetFileReader TweetFileReader com a lista de Tweets já gerada
+     */
+    public static void benchmarkInsertTweets(BenchmarkableTree<Tweet> tree, int[] nValues, TweetFileReader tweetFileReader) {
+        System.out.println("\n\n--- Inserção ---\n");
+        assert tweetFileReader != null;
+        for (int n : nValues) {
+            System.out.println("N: " + n);
+            Tweet[][] tweetArrays = generateTweetArrays(n, tweetFileReader.getTweets());
+            long comparisonsSum = 0;
+            long copiesSum = 0;
+            long timeSpentSum = 0;
+            for (Tweet[] tweets : tweetArrays) {
+                tree.clear();
+                tree.start();
+                for (Tweet tweet : tweets)
+                    tree.insert(tweet);
+                comparisonsSum += tree.getComparisons();
+                copiesSum += tree.getCopies();
+                timeSpentSum += tree.getTimeSpent();
+            }
+            System.out.println("\tNúmero médio de comparações: " + (comparisonsSum / AMOUNT_OF_SETS));
+            System.out.println("\tNúmero médio de cópias de registros: " + (copiesSum / AMOUNT_OF_SETS));
+            System.out.println(String.format("\tTempo médio gasto: %d milisegundos\n", (timeSpentSum / AMOUNT_OF_SETS)));
+        }
+    }
+
+    /**
+     * Método que analisa a busca em uma estrutura de árvore com os valores dados de entrada.
+     *
+     * A array nValues contém os valores de N (quantidade de elementos a serem buscados)
+     * a serem utilizados na análise.
+     *
+     * A variável AMOUNT_OF_SETS é utilizada para determinar quantos testes
+     * serão realizados com diferentes sementes e contabilizados na média
+     * dos resultados obtidos.
+     *
+     * @param tree Estrutura de árvore a ser analisada
+     * @param nValues Valores de N (tamanho da array)
+     * @param tweetFileReader TweetFileReader com a lista de Tweets já gerada
+     */
+    public static void benchmarkSearchTweets(BenchmarkableTree<Tweet> tree, int[] nValues, TweetFileReader tweetFileReader) {
+        System.out.println("\n\n--- Busca ---\n");
+        assert tweetFileReader != null;
+        for (int n : nValues) {
+            System.out.println("N: " + n);
+            Tweet[][] tweetArrays = generateTweetArrays(n, tweetFileReader.getTweets());
+            long comparisonsSum = 0;
+            long copiesSum = 0;
+            long timeSpentSum = 0;
+            for (Tweet[] tweets : tweetArrays) {
+                tree.clear();
+                for (Tweet tweet : tweets)
+                    tree.insert(tweet);
+                tree.start();
+                for (Tweet tweet : tweets)
+                    tree.search(tweet);
+                comparisonsSum += tree.getComparisons();
+                copiesSum += tree.getCopies();
+                timeSpentSum += tree.getTimeSpent();
+            }
+            System.out.println("\tNúmero médio de comparações: " + (comparisonsSum / AMOUNT_OF_SETS));
+            System.out.println("\tNúmero médio de cópias de registros: " + (copiesSum / AMOUNT_OF_SETS));
+            System.out.println(String.format("\tTempo médio gasto: %d milisegundos\n", (timeSpentSum / AMOUNT_OF_SETS)));
+        }
+    }
+
+    /**
+     * Método que analisa a remoção em uma estrutura de árvore com os valores dados de entrada.
+     *
+     * A array nValues contém os valores de N (quantidade de elementos a serem removidos)
+     * a serem utilizados na análise.
+     *
+     * A variável AMOUNT_OF_SETS é utilizada para determinar quantos testes
+     * serão realizados com diferentes sementes e contabilizados na média
+     * dos resultados obtidos.
+     *
+     * @param tree Estrutura de árvore a ser analisada
+     * @param nValues Valores de N (tamanho da array)
+     * @param tweetFileReader TweetFileReader com a lista de Tweets já gerada
+     */
+    public static void benchmarkRemoveTweets(BenchmarkableTree<Tweet> tree, int[] nValues, TweetFileReader tweetFileReader) {
+        System.out.println("\n\n--- Remoção ---\n");
+        assert tweetFileReader != null;
+        for (int n : nValues) {
+            System.out.println("N: " + n);
+            Tweet[][] tweetArrays = generateTweetArrays(n, tweetFileReader.getTweets());
+            long comparisonsSum = 0;
+            long copiesSum = 0;
+            long timeSpentSum = 0;
+            for (Tweet[] tweets : tweetArrays) {
+                tree.clear();
+                for (Tweet tweet : tweets)
+                    tree.insert(tweet);
+                tree.start();
+                for (Tweet tweet : tweets)
+                    tree.remove(tweet);
+                comparisonsSum += tree.getComparisons();
+                copiesSum += tree.getCopies();
+                timeSpentSum += tree.getTimeSpent();
+            }
+            System.out.println("\tNúmero médio de comparações: " + (comparisonsSum / AMOUNT_OF_SETS));
+            System.out.println("\tNúmero médio de cópias de registros: " + (copiesSum / AMOUNT_OF_SETS));
+            System.out.println(String.format("\tTempo médio gasto: %d milisegundos\n", (timeSpentSum / AMOUNT_OF_SETS)));
+        }
+    }
+}
