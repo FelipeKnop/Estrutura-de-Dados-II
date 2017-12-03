fa48ccb8804305471590741359108ba8b8168492
Felipe Knop <felipebarraknop@yahoo.com.br>
2017-12-03 13:36:11 -0200

    Simplificação dos algoritmos LZ78 e LZW

--- src/compression/LZWCompressionAlgorithm.java
+++ src/compression/LZWCompressionAlgorithm.java
@@ -1,44 +1,24 @@
 package compression;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class LZWCompressionAlgorithm implements CompressionAlgorithm {
 
-    // TODO
     @Override
-    public void compress(String content, BinaryOutputStream output) {
-        char c ;
-        //Iniciando dicionario com caracteres basicos da tabela ASCII
-        Map<String,Integer>  dicionario = new HashMap<>();
-        for(int z=0; z<=255; z++)
-        {
-            c = (char) z;
-            dicionario.put(new String(""+c),z);
-        }
-        String oldReader="" + content.charAt(0);
-        String reader = "" ;
-        int count = 1, i = 1;
-        int tam = content.length();
-        while(i < tam)
-        {
-
-            if( content.charAt(i)<=255 ) {   //Verifica se caracter pertence a valores basicos da tabela ASCII
-                reader = oldReader + content.charAt(i);
-                if (reader.length() > 1) {
-                    if (!dicionario.containsKey(reader)) {
-                        dicionario.put(reader, 255 + count);
-                        count++;
-                        output.write(dicionario.get(oldReader));
-                        oldReader = "" + content.charAt(i);
-                    } else oldReader = reader;
-
-                }
+    public void compress(String content, BinaryOutputStream out) {
+        String string = String.valueOf(content.charAt(0));
+        content = content.substring(1);
+        char[] charContent = content.toCharArray();
+        ArrayList<String> dict = new ArrayList<>();
+        for (char c : charContent) {
+            String aux = string + c;
+            if (dict.contains(aux)) {
+                string = aux;
+            } else {
+                out.write(dict.indexOf(string), 24);
+                dict.add(aux);
+                string = String.valueOf(c);
             }
-           i++;
-
         }
-
-
     }
 }
